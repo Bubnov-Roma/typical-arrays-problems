f1a376c0d63f2eb44a9f84d5cc6dbe80bbc1db23
Bubnov-Roma <kilkun@mail.ru>
2020-10-01 22:54:24 +0400

    feat: solve array algorithms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array && array.length) {
+        return array.reduce(function (p, v) {
+            return ( p < v ? p : v );
+          });
+      } else {
+          return 0;
+      };
+    }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if (array && array.length) {
+        var max = array[0];
+    for(var i = 0; i < array.length; i++) {
+            if(array[i] > max) 
+            max = array[i];
+        }
+        return max;
+    } else {
+        return 0;
+    }
+
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if (array && array.length) {
+        return array.reduce((a, b) => a + b,0) / array.length;
+    } else {
+        return 0;
+    }
 }
